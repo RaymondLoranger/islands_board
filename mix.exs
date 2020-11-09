91da29cab389dbc5685737be7517bf6d800b86ea
RaymondLoranger <Raymond.Loranger@bell.net>
2020-11-09 07:26:21 -0500

    0.1.15

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Board.MixProject do
   def project do
     [
       app: :islands_board,
-      version: "0.1.14",
+      version: "0.1.15",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Board",
@@ -46,7 +46,6 @@ defmodule Islands.Board.MixProject do
     [
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false},
-      {:islands_config, "~> 0.1", runtime: false},
       {:islands_coord, "~> 0.1"},
       {:islands_island, "~> 0.1"},
       {:jason, "~> 1.0"},
